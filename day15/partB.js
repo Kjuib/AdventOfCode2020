c94f492671732369c9a9e1a4745d1045b25f1c81
Heber Billings <heber.billings@gmail.com>
2020-12-15 08:44:55 -0700

    day15 BUT WAY QUICKER

--- day15/partB.js
+++ day15/partB.js
@@ -1,25 +1,25 @@
 const _ = require('lodash');
 
-const input = [20,9,11,0,1,2];
+const input = [0,3,6];
 const iterations = 30_000_000;
 
 function main() {
     const list = _.slice(input, 0, _.size(input) - 1);
     let current = _.last(input);
 
-    const lastIndexList = {};
+    const lastIndexList = new Map();
     _.forEach(list, (item, index) => {
-        lastIndexList[item] = index;
+        lastIndexList.set(item, index);
     });
 
     for (let i = _.size(list); i < (iterations - 1); i++) {
-        const lastIndex = lastIndexList[current];
+        const lastIndex = lastIndexList.get(current);
 
-        lastIndexList[current] = i;
+        lastIndexList.set(current, i);
         current = _.isNil(lastIndex) ? 0 : i - lastIndex;
 
         if (i % 100000 === 0) {
-            console.log('Checking...', i, current, _.size(lastIndexList));
+            console.log('Checking...', i, current);
         }
     }
     console.log('current', current);
